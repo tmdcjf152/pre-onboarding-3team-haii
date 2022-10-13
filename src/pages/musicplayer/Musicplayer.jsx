5ff68e1140a2c03fb2618c8750db7d6dc3be9eb8
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-10-13 10:48:53 +0900

    [Pull] feature/layout

--- src/pages/musicplayer/Musicplayer.jsx
+++ src/pages/musicplayer/Musicplayer.jsx
@@ -1,18 +1,12 @@
 import styled from "styled-components";
 
 const Musicplayer = () => {
-<<<<<<< HEAD
   return <MusicplayerBlock></MusicplayerBlock>;
-=======
-  return (
-    <MusicplayerBlock>음악재생화면</MusicplayerBlock>
-  );
->>>>>>> a93fc621b1821dce3afd8d5637012d54c4aad8b5
 };
 const MusicplayerBlock = styled.div`
   margin-left: 300px;
-      @media (max-width: 768px) {
-        margin-left: 0;
-    }
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
 `;
 export default Musicplayer;
